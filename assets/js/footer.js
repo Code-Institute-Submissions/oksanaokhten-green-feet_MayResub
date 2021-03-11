1a005bdec1572a39407792025d8c39e472a71379
oksanaokhten <oksanaokhten@yahoo.com>
2021-03-11 18:52:58 +0000

    Page loaded time was displayed in footer, style.css was changed for front page.

---
+++ assets/js/footer.js
@@ -0,0 +1,10 @@
+//Code was taken from Java Script tutorial
+
+document.addEventListener("DOMContentLoaded", function() {
+    displayTime();
+    function displayTime() {
+        let el = document.getElementById("time");
+        let now = new Date();
+        el.innerText = now;
+    }
+})

aea4a8b47ef070ca9e2b80c79f10b38d340abb95
oksanaokhten <oksanaokhten@yahoo.com>
2021-04-21 12:19:23 +0000

    Yellow text colour was removed from Dark mode, Charity abd Send buttons are changed their class to light and info in Dark mode.

--- assets/js/mode.js
+++ assets/js/mode.js
@@ -2,31 +2,31 @@ function changingMode() {
     let el = document.getElementById("mode");
     if (el.innerText == "Dark mode") {
         el.innerText = "Light mode";
-        darkMode();
+        darkMode();  
     } else {
         el.innerText = "Dark mode";
-        lightMode();
+        lightMode();     
     }
-}
+};
 
 function darkMode() {
     $("nav").removeClass("bg-light").removeClass("navbar-light").addClass("bg-dark").addClass("navbar-dark");
     $(".cream-overlay").css("background-color", "rgba(0, 0, 0, 0.5)");
     $("#about").removeClass("bg-light").addClass("bg-dark").css("color", "#fafafa");
-    $(".charity-btn").css("color","#fafafa");
+    $(".charity-btn").removeClass("btn-outline-success").addClass("btn-light");
     $("#location").removeClass("bg-location").addClass("bg-location-dark");
     $("#contact").removeClass("bg-light").addClass("bg-dark").css("color", "#fafafa");
-    $(".form-btn").css("color", "#fafafa");
-    $("#footer").removeClass("bg-footer").addClass("bg-footer-dark");
+    $(".form-btn").removeClass("btn-outline-success").addClass("btn-info");
+    $("#footer").removeClass("bg-footer").addClass("bg-footer-dark");   
 };
 
 function lightMode() {
     $("nav").removeClass("bg-dark").removeClass("navbar-dark").addClass("bg-light").addClass("navbar-light");
     $(".cream-overlay").css("background-color", "rgba(17, 17, 16, 0.4)");
     $("#about").removeClass("bg-dark").addClass("bg-light").css("color", "#212529");
-    $(".charity-btn").css("color","#212529");
+    $(".charity-btn").removeClass("btn-light").addClass("btn-outline-success");
     $("#location").removeClass("bg-location-dark").addClass("bg-location");
     $("#contact").removeClass("bg-dark").addClass("bg-light").css("color", "#212529");
-    $(".form-btn").css("color","#212529");
+    $(".form-btn").removeClass("btn-info").addClass("btn-outline-success");
     $("#footer").removeClass("bg-footer-dark").addClass("bg-footer");
-}
+};

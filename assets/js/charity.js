51f3207a5e48bcd0f735bec4e44c56297c20235e
oksanaokhten <oksanaokhten@yahoo.com>
2021-04-28 13:41:16 +0000

    Added more interactivity to About section with customer input.

--- assets/js/charity.js
+++ assets/js/charity.js
@@ -1,6 +1,29 @@
-function clientPartOfCharity (clientPayment, partOfPayment) {
-    let amount = clientPayment * partOfPayment;
-    //Get element with id of toys
-    let el = document.getElementById("toys");
-    el.innerText = `After each of your payments, ${amount} € go to the Fundraising.`;
+function charity() {
+    let button = document.getElementById("charity");
+    button.addEventListener("click", function() {
+        amountCalculation(50, 0.075);
+    });
 };
+
+function amountCalculation(clientPayment, partOfPayment) {
+    let userAmount = parseInt(document.getElementById("input-amount").value);
+    let amount = clientPayment * partOfPayment * userAmount;
+    let el = document.getElementById("donation");
+    el.innerText = `To the Fundraising went ${amount} euro.`;
+};
+
+charity();
+
+// This code was taken from JS-Essential-Project and changed for my project needs.
+function helpKids() {
+    document.getElementById("input-amount").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            amountCalculation(50, 0.075);
+        };
+    });
+
+    document.getElementById("input-amount").value = ""; // Erases the last typed answer
+    document.getElementById("input-amount").focus(); // Puts the cursor in the answer box
+};
+
+helpKids();

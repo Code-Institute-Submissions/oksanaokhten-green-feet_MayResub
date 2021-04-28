5cf41b93b4d07cbd9998315a4c74090116482cff
oksanaokhten <oksanaokhten@yahoo.com>
2021-04-28 11:24:32 +0000

    Interactivity was added to the footer.

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,10 +1,29 @@
-//Code was taken from Java Script tutorial
+function whenArrive() {
+    let button = document.getElementById("arrive");
+    button.addEventListener("click", function() {
+        displayTime();
+    });
+};
 
-document.addEventListener("DOMContentLoaded", function() {
-    displayTime();
-    function displayTime() {
-        let el = document.getElementById("time");
-        let now = new Date();
-        el.innerText = now;
-    }
-});
+function closeButton() {
+    let button = document.getElementById("close");
+    button.addEventListener("click", function() {
+        removeText();
+    })
+}
+//Code was taken from Java Script tutorial and changed for this project needs.
+
+function displayTime() {
+    let el = document.getElementById("time");
+    let now = new Date();
+    el.innerText = `You arrived on ${now}`;
+};
+
+function removeText() {
+    let el = document.getElementById("time");
+    el.innerText = ``;
+};
+
+whenArrive();
+
+closeButton();

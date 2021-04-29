d88489da0b32a21916221ff208dee7cd265d7a79
oksanaokhten <oksanaokhten@yahoo.com>
2021-04-29 20:28:13 +0000

    frontPage.js code was covered into function.

--- assets/js/frontPage.js
+++ assets/js/frontPage.js
@@ -1,3 +1,11 @@
-const element = document.querySelector(".bring-to-attention");
-element.classList.add('animate__animated', 'animate__flash');
-element.style.setProperty('--animate-duration', '6s');
+// The code inside of function below was taken from Animate.css library. 
+// It was done with JavaScript to change a duration time.
+
+function reflexologyTreatment() {
+    const element = document.querySelector(".bring-to-attention");
+    element.classList.add('animate__animated', 'animate__flash');
+    element.style.setProperty('--animate-duration', '6s');
+};
+
+reflexologyTreatment();
+
